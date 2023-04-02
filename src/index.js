8f00fdbf4e7c3e68f8353a339b1080ce6d809b27
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2023-04-02 13:47:33 -0300

    [ADD] WEBP images of pages added into assets folder

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-/*
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "./services/keys/firebase";
 
 initializeApp(firebaseConfig);
-*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
