62df17826c4c41c9c5bf6f7b004f19e57de918e6
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2023-04-09 12:01:02 -0300

    [ADD] Netlify server configuration

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import { initializeApp } from "firebase/app";
-import firebaseConfig from "./services/keys/firebase";
+import firebaseConfig from "./services/prod_keys/firebase";
+import {initializeFirestore} from 'firebase/firestore';
 
-initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+initializeFirestore(firebaseApp, {
+  experimentalForceLongPolling: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
