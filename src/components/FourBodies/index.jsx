a2378aedc606e2c5bf916611bc6132668604a276
santif <santiagofonzo@live.com>
2023-04-10 19:26:45 -0300

    [FIX] Navbar expanded and bodies container

--- src/components/FourBodies/index.jsx
+++ src/components/FourBodies/index.jsx
@@ -1,14 +1,13 @@
 import ItemsList from "../ItemsList";
 import "./styles.scss";
-import { Container } from "react-bootstrap";
 
 const FourBodies = () => {
     const itemsNames = ["mental", "physical_therapy", "mood", "spiritual"];
     return (
         <section className="four-bodies">
-            <Container>
+            <div className="container-lg">
                 <ItemsList itemsNames={itemsNames} design={"white-rounded"} cardsAreLinks={false} />
-            </Container>
+            </div>
         </section>
     );
 }
