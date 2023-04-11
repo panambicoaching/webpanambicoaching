d1e7a53d7f66d9434a32a8e185dbf0c7b8886e4a
Ignacio Gutierrez <dev.ignaciogz@gmail.com>
2023-04-10 22:49:03 -0300

    [ADD] Open Graph Protocol and Twitter Card

--- src/pages/WorkshopsPage.jsx
+++ src/pages/WorkshopsPage.jsx
@@ -7,7 +7,7 @@ const WorkshopsPage = () => {
 		return (
             <>
                 <Helmet>
-                    <title>Talleres de coaching | Panambí coaching</title>
+                    <title>Talleres de coaching profesional y especializado | Panambí coaching</title>
                     <link href="https://panambicoaching.com/" rel="canonical" />
                     <meta
                         content="Talleres de coaching profesional presenciales y online, totalmente personalizados con material teórico y práctico, y certificado de participación. Orientados a crear metodologías de trabajo, brindar herramientas de desarrollo para mejorar las relaciones laborales, incentivando una competencia sana y generando un clima organizacional más ameno. Generarndo no sólo un cambio organizacional, sino además social, del círculo inmediato y externo de las personas."
